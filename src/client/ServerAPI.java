e178f6a68612865720ef746ac2c0272aa49db4e3
XY-e <g1083415575@gmail.com>
2016-12-06 15:11:31 +0800

    添加一部分连接服务器的功能

--- src/client/ServerAPI.java
+++ src/client/ServerAPI.java
@@ -1,102 +1,241 @@
 package client;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.codehaus.jackson.map.ObjectMapper;
 
 //定义服务器接口，处理服务器和数据库相关事务
+@SuppressWarnings("deprecation")
 public class ServerAPI {
-
+    private static ObjectMapper objectMapper = new ObjectMapper();
+    private static String token;
+    private static String baseUrl = "http://127.0.0.1/";
     /**
-     * TODO:从数据库中检查用户名和密码是否匹配
+     * 从数据库中检查用户名和密码是否匹配
      * @param username 用户名
      * @param password 密码
      * @return 匹配则返回true， 否则返回false
      */
     public static boolean checkPassword(String username, String password) {
-        return true;
-    }
-
-
-    /**
-     * TODO: 遍历数据库，看该username是否已经存在
-     * @param username 用户名
-     * @return 若该username存在则返回true，否则返回false
-     */
-    public static boolean userNameExist(String username){
+        List<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("username", username));
+        params.add(new BasicNameValuePair("password", password));
+        HttpResponse response = post(baseUrl + "login", params);
+        if(response == null)
+            return false;
+        if (response.getStatusLine().getStatusCode() == 200)
+        {
+            String str;
+            try {
+                str = EntityUtils.toString(response.getEntity());
+                Map resMap = objectMapper.readValue(str, Map.class);
+                if((int)resMap.get("status") == 0){
+                    token = (String) resMap.get("token");
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
         return false;
     }
 
 
     /**
-     * TODO: 注册成功，将用户数据写入数据库
+     *  遍历数据库，看该username是否已经存在
      * @param username 用户名
-     * @param password 密码
+     * @return 若该username存在则返回true，否则返回false
      */
-    public static void saveUserData(String username, String password) {
+    public static boolean userNameExist(String username){
+        HttpResponse response = get(baseUrl + "checkUser?username=" + username);
+        if(response == null)
+            return true;
+        if (response.getStatusLine().getStatusCode() == 200)
+        {
+            String str;
+            try {
+                str = EntityUtils.toString(response.getEntity());
+                Map resMap = objectMapper.readValue(str, Map.class);
+                return (int) resMap.get("status") != 0;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return true;
+            }
+        }
+        return true;
     }
 
 
     /**
-     * TODO: 将用户状态设置为在线
+     * 注册成功，将用户数据写入数据库
      * @param username 用户名
+     * @param password 密码
      */
-    public static void userOnline(String username){}
+    public static void saveUserData(String username, String password) {
+        List<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("username", username));
+        params.add(new BasicNameValuePair("password", password));
+        HttpResponse response = post(baseUrl + "register", params);
+        assert response != null;
+
+        if (response.getStatusLine().getStatusCode() == 200)
+        {
+            String str;
+            try {
+                str = EntityUtils.toString(response.getEntity());
+                Map resMap = objectMapper.readValue(str, Map.class);
+                if((int)resMap.get("status") == 0)
+                    System.out.println("register successfully");
+                else
+                    System.out.println((String)resMap.get("msg"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 
     /**
-     * TODO:将用户状态设置为离线
+     * 将用户状态设置为在线
      * @param username 用户名
      */
-    public static void userOffline(String username){}
+    public static void userOnline(String username){
+    }
 
 
     /**
-     * TODO:从数据库中获取当前用户对当前单词的点赞情况
+     * 将用户状态设置为离线
+     * @param username 用户名
+     */
+    public static void userOffline(String username) throws SomeException {
+        HttpResponse response = get(baseUrl + "logout?token=" + token);
+        token = "";
+        if (response == null || response.getStatusLine().getStatusCode() != 200)
+            throw new SomeException("无法访问服务器");
+    }
+
+
+    /**
+     * 从数据库中获取当前用户对当前单词的点赞情况
      * @param userName 用户名
      * @param word 单词
      * @return 长度为3的ArrayList，依次是百度，有道，必应的点赞情况，点过赞则为true，否则为false
      */
     public static List<Boolean> getUserFavour(String userName, String word) {
         ArrayList<Boolean> userFavour = new ArrayList<>();
-        //下面代码需要被替换
-        userFavour.add(true);
         userFavour.add(false);
-        userFavour.add(true);
-
+        userFavour.add(false);
+        userFavour.add(false);
+        HttpResponse response = get(baseUrl + "getLiked?token=" + token + "&word=" + word);
+        if(response == null)
+            return userFavour;
+        if (response.getStatusLine().getStatusCode() == 200)
+        {
+            String str;
+            try {
+                str = EntityUtils.toString(response.getEntity());
+                Map resMap = objectMapper.readValue(str, Map.class);
+                if((int)resMap.get("status") == 0)
+                {
+                    userFavour.add((boolean)resMap.get("bd"));
+                    userFavour.add((boolean)resMap.get("yd"));
+                    userFavour.add((boolean)resMap.get("by"));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         return userFavour;
     }
 
 
     /**
-     * TODO:从数据库获取某单词的点赞数
+     * 从数据库获取某单词的点赞数
      * @param word 该单词
      * @return 长度为3的ArrayList,依次是百度，有道，必应的点赞数
      */
     public static ArrayList<Integer> getFavoursNum(String word) {
-        ArrayList<Integer> favoursNum = new ArrayList<Integer>(3);
-        //下面代码需要被替换
-        favoursNum.add(1);
-        favoursNum.add(2);
-        favoursNum.add(3);
+        ArrayList<Integer> favoursNum = new ArrayList<>(3);
+        favoursNum.add(0);
+        favoursNum.add(0);
+        favoursNum.add(0);
+        System.out.println("before");
+        HttpResponse response = get(baseUrl + "getRank?token=" + token + "&word=" + word);
+        if(response == null)
+            return favoursNum;
+        if (response.getStatusLine().getStatusCode() == 200)
+        {
+            String str;
+            try {
+                str = EntityUtils.toString(response.getEntity());
+                Map resMap = objectMapper.readValue(str, Map.class);
+                if((int)resMap.get("status") == 0)
+                {
+                    favoursNum.add((int)resMap.get("bd"));
+                    favoursNum.add((int)resMap.get("yd"));
+                    favoursNum.add((int)resMap.get("by"));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
         return favoursNum;
     }
 
 
     /**
-     * TODO:从数据库获取用户在线信息
+     * 从数据库获取用户在线信息
      * @return HaspMap中每个元素代表一个用户，String为用户名，Boolean表示用户是否在线，在线为true，否则为false
      */
     public static HashMap<String, Boolean> getAllUsers() {
         HashMap<String, Boolean> allUsers = new HashMap<>();
-        //下面代码需要被替换
-        allUsers.put("Alice", true);
-        allUsers.put("Bob", false);
-        allUsers.put("Chris", true);
 
+        String types[] = {"getOfflineUsers", "getOnlineUsers"};
+        boolean t = false;
+        for(String type: types){
+            String url = baseUrl + type + "?token=" + token;
+            HttpResponse response = get(url);
+            if(response == null)
+                continue;
+            if (response.getStatusLine().getStatusCode() == 200)
+            {
+                String str;
+                try {
+                    str = EntityUtils.toString(response.getEntity());
+                    Map resMap = objectMapper.readValue(str, Map.class);
+                    if((int)resMap.get("status") == 0)
+                    {
+                        String allUserString = (String)resMap.get("users");
+                        String allUserArray[] = allUserString.split(" ");
+                        for(String userInfo: allUserArray){
+                            String userName = userInfo.split("-")[1];
+                            allUsers.put(userName, t);
+                        }
+                        t = true;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return allUsers;
     }
 
@@ -110,4 +249,30 @@ public class ServerAPI {
      */
     public static void shareWordCard(String word, File file, String sender, String[] receiver){}
 
+    private static HttpResponse post(String url, List<NameValuePair> params){
+
+        HttpClient httpclient = new DefaultHttpClient();
+
+        HttpPost httpPost = new HttpPost(url);
+        try
+        {
+            httpPost.setEntity(new UrlEncodedFormEntity(params));
+
+            return httpclient.execute(httpPost);
+        } catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    private static HttpResponse get(String url){
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            return httpclient.execute(httpGet);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
 }
